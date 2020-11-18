6b933967e9fc1c13fd5e4df1798e83634f9a57a0
sanketnaik <sanketnaik@arubanetworks.com>
2020-11-18 10:11:34 +0000

    licensing

---
+++ licensing/src/main/java/com/elixir/licensing/LicensingApplication.java
@@ -0,0 +1,13 @@
+package com.elixir.licensing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensingApplication.class, args);
+	}
+
+}
